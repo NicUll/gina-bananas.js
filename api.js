e6aa5df23ccc38f175521b9ac1045735aeba4c10
r0binnilss0ns <robin.nilsson@ginatricot.com>
2026-06-22 16:05:57 +0200

    update csrftoken name

--- api.js
+++ api.js
@@ -51,7 +51,7 @@ class APIClient extends _swaggerClient.default {
   }
 
   http(request) {
-    var csrftoken = (0, _utils.getCookie)("csrftoken");
+    var csrftoken = (0, _utils.getCookie)("csrftoken_v2");
     return super.http(_objectSpread(_objectSpread({}, request), {}, {
       headers: _objectSpread({
         "X-CSRFToken": csrftoken == null ? "" : csrftoken
